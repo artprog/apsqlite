1f469f40a46082da17ea5c68ea8be0be8950e661
Adam Zugaj <adam.zugaj@artprog.pl>
2015-06-30 14:17:55 +0200

    added project

---
+++ apsqlite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  apsqlite
+//
+//  Created by Adam Zugaj on 30/06/15.
+//  Copyright (c) 2015 Adam Zugaj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
